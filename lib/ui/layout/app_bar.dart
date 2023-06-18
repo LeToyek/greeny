f2baa4ac19cb189f6fcbd4a3ebbd52421bbc5894
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-06-18 17:08:30 +0700

    feat: init app bar

--- lib/ui/layout/app_bar.dart
+++ lib/ui/layout/app_bar.dart
@@ -16,18 +16,10 @@ class GrAppBar extends StatelessWidget implements PreferredSizeWidget {
         statusBarIconBrightness:
             brightness == Brightness.dark ? Brightness.light : Brightness.dark,
       ),
-      backgroundColor: Colors.transparent,
-      excludeHeaderSemantics: true,
-      shadowColor: Colors.transparent,
-      scrolledUnderElevation: 0,
-      surfaceTintColor: Colors.transparent,
-      foregroundColor: Colors.transparent,
-      elevation: 0,
-      bottomOpacity: 0,
-      toolbarOpacity: 0,
     );
   }
 
   @override
+  // TODO: implement preferredSize
   Size get preferredSize => const Size.fromHeight(8);
 }

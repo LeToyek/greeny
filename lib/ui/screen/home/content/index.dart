cf7ecfce67e366c787ba0f0007053faf858062ea
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-06-18 17:07:50 +0700

    feat: add content list

---
+++ lib/ui/screen/home/content/index.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:greenify/ui/screen/home/content/account_screen.dart';
+import 'package:greenify/ui/screen/home/content/home_screen.dart';
+
+final List<Widget> contents = [const HomeScreen(), const AccountScreen()];

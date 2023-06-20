68f788142638463924ffcf90803798a4fb8e1e08
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-06-20 21:53:03 +0700

    feat: init book page

--- lib/ui/screen/book/book_screen.dart
+++ lib/ui/screen/book/book_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:greenify/ui/widgets/card/info_card.dart';
+import 'package:ionicons/ionicons.dart';
 
 class BookScreen extends StatelessWidget {
   const BookScreen({super.key});
@@ -8,10 +10,44 @@ class BookScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: const [],
-        title: const Text("Book"),
+        centerTitle: true,
+        title: const Text(
+          "Book",
+        ),
       ),
-      body: const Center(
-        child: Text("test"),
+      body: Material(
+        color: Theme.of(context).colorScheme.background,
+        child: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            physics: const BouncingScrollPhysics(),
+            children: [
+              GridView.count(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                childAspectRatio: 4 / 5.5,
+                padding: EdgeInsets.zero,
+                children: const <GrInfoCard>[
+                  GrInfoCard(
+                      title: 'Media Tanam',
+                      content: 'localization_content',
+                      icon: Ionicons.leaf_outline,
+                      isPrimaryColor: false),
+                  GrInfoCard(
+                      title: 'Jenis Tanaman',
+                      content: 'linting_content',
+                      icon: Ionicons.flower_outline,
+                      isPrimaryColor: false),
+                  GrInfoCard(
+                      title: 'Perawatan',
+                      content: 'storage_content',
+                      icon: Ionicons.heart_outline,
+                      isPrimaryColor: false),
+                ],
+              ),
+            ]),
       ),
     );
   }

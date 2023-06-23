0a4e849ca99d657469e8e5f5ea2901add073d9d3
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-06-23 13:42:24 +0700

    feat: add card state

--- lib/ui/widgets/card/plant_card.dart
+++ lib/ui/widgets/card/plant_card.dart
@@ -1,36 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+enum PlantBoxStatus { empty, filled, locked }
 
 class PlantCard extends StatelessWidget {
-  const PlantCard({super.key, required this.title, required this.imageURI});
+  PlantCard(
+      {super.key,
+      required this.title,
+      required this.imageURI,
+      this.status = PlantBoxStatus.locked});
 
   final String title;
   final String imageURI;
+  late PlantBoxStatus status;
 
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
     return Card(
-      elevation: 2,
-      shadowColor: Theme.of(context).colorScheme.shadow,
-      color: Theme.of(context).colorScheme.surface,
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(12))),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageURI),
-            const Spacer(),
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: textTheme.titleSmall!.apply(fontWeightDelta: 2),
-            ),
-          ],
+        elevation: 2,
+        shadowColor: Theme.of(context).colorScheme.shadow,
+        color: Theme.of(context).colorScheme.surface,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12))),
+        child: _buildPlantBox(status, textTheme, title, imageURI));
+  }
+
+  Widget _buildPlantBox(
+      PlantBoxStatus status, TextTheme textTheme, String title, String imgUrl) {
+    switch (status) {
+      case PlantBoxStatus.empty:
+        return PlantBox(textTheme, "Tambah", Ionicons.add_circle_outline, null);
+      case PlantBoxStatus.filled:
+        return PlantBox(textTheme, title, null, imgUrl);
+      case PlantBoxStatus.locked:
+        return PlantBox(
+            textTheme, "Locked", Ionicons.lock_closed_outline, null);
+    }
+  }
+
+  Widget PlantBox(
+      TextTheme textTheme, String title, IconData? icon, String? imageURI) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        const Spacer(),
+        imageURI != null ? Image.asset(imageURI) : Icon(icon),
+        const Spacer(),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: textTheme.titleSmall!.apply(fontWeightDelta: 2),
         ),
-      ),
+        const SizedBox(height: 10),
+      ],
     );
   }
 }

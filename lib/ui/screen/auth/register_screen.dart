3f8c1a8938db27376faa694b13135688d18eb809
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-07-11 23:06:44 +0700

    feat: init loading

--- lib/ui/screen/auth/register_screen.dart
+++ lib/ui/screen/auth/register_screen.dart
@@ -1,19 +1,25 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:greenify/services/auth.dart';
 
-class RegisterScreen extends ConsumerWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
     final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
     TextEditingController emailController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
     TextEditingController nameController = TextEditingController();
+
+    late bool isLoading = false;
 
     void _submitForm() async {
       try {
@@ -23,9 +29,14 @@ class RegisterScreen extends ConsumerWidget {
           String email = emailController.text;
           String password = passwordController.text;
 
+          setState(() {
+            isLoading = true;
+          });
           User? user = await FireAuth.registerUser(
               email: email, password: password, name: name);
-
+          setState(() {
+            isLoading = false;
+          });
           if (user != null) {
             if (context.mounted) {
               context.pushReplacement("/");
@@ -42,7 +53,10 @@ class RegisterScreen extends ConsumerWidget {
                   content: Text(e.toString()),
                   actions: [
                     TextButton(
-                        onPressed: () => context.pop(), child: const Text("OK"))
+                        onPressed: () {
+                          context.pop();
+                        },
+                        child: const Text("OK"))
                   ],
                 ));
       }
@@ -50,53 +64,61 @@ class RegisterScreen extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Register')),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: nameController,
-                  decoration: const InputDecoration(labelText: 'Name'),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
+      body: Stack(
+        children: [
+          isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : const SizedBox.shrink(),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: nameController,
+                      decoration: const InputDecoration(labelText: 'Name'),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your name';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(labelText: 'Email'),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: const InputDecoration(labelText: 'Password'),
+                      obscureText: true,
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: _submitForm,
+                      child: const Text('Register'),
+                    ),
+                  ],
                 ),
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(labelText: 'Email'),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16.0),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: const Text('Register'),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }

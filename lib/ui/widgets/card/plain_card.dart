cf16e38459139bbc6172aac591763c6c26ee422a
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-07-11 14:45:30 +0700

    feat: init plain card

---
+++ lib/ui/widgets/card/plain_card.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PlainCard extends StatelessWidget {
+  final Widget child;
+  Color? color;
+  PlainCard({super.key, required this.child, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      decoration: BoxDecoration(
+        color: color ?? Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).colorScheme.onSurface.withOpacity(0.1),
+            blurRadius: 8,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}

01a5d371a97ad9b21b43f266866423a15dc364bc
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-07-12 16:54:51 +0700

    feat: add image storage to user

--- lib/states/users_state.dart
+++ lib/states/users_state.dart
@@ -3,12 +3,36 @@ import 'package:greenify/model/user_model.dart';
 import 'package:greenify/services/users.dart';
 
 class UsersNotifier extends StateNotifier<AsyncValue<List<UserModel>>> {
-  UsersNotifier() : super(const AsyncValue.loading());
+  UsersServices usersServices;
+  UsersNotifier({required this.usersServices})
+      : super(const AsyncValue.loading());
   Future<void> getUsers() async {
     try {
-      print("testt");
       final users = await UsersServices().getUsers();
       state = AsyncValue.data(users);
+    } catch (e) {
+      state = AsyncValue.error(e, StackTrace.current);
+    }
+  }
+
+  Future<void> getUser() async {
+    try {
+      final user = await usersServices.getUser();
+
+      state = AsyncValue.data([user]);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<void> updateProfilePhoto(String? oldUrl) async {
+    try {
+      print("oldUrl => $oldUrl");
+      state = const AsyncLoading();
+      String photoUrl = await UsersServices().updateProfilePhoto(oldUrl);
+      UserModel user = await usersServices.getUser();
+      user.imageUrl = photoUrl;
+      state = AsyncValue.data([user]);
     } catch (e) {
       state = AsyncValue.error(e, StackTrace.current);
     }
@@ -17,4 +41,8 @@ class UsersNotifier extends StateNotifier<AsyncValue<List<UserModel>>> {
 
 final usersProvider =
     StateNotifierProvider<UsersNotifier, AsyncValue<List<UserModel>>>(
-        (ref) => UsersNotifier()..getUsers());
+        (ref) => UsersNotifier(usersServices: UsersServices())..getUsers());
+
+final singleUserProvider =
+    StateNotifierProvider<UsersNotifier, AsyncValue<List<UserModel>>>(
+        (ref) => UsersNotifier(usersServices: UsersServices())..getUser());
--- lib/ui/screen/home/content/account_screen.dart
+++ lib/ui/screen/home/content/account_screen.dart
@@ -1,79 +1,132 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:greenify/services/auth.dart';
+import 'package:greenify/states/user_action_state.dart';
+import 'package:greenify/states/users_state.dart';
 import 'package:greenify/ui/layout/header.dart';
 import 'package:greenify/ui/widgets/card/plain_card.dart';
 import 'package:ionicons/ionicons.dart';
 
-class AccountScreen extends StatelessWidget {
+class AccountScreen extends ConsumerWidget {
   const AccountScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final User? user = FireAuth.getCurrentUser();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userAction = ref.watch(userActionProvider);
+    final funcUserAction = ref.read(userActionProvider.notifier);
+
+    final userRef = ref.watch(singleUserProvider);
+    final funcUserRef = ref.read(singleUserProvider.notifier);
 
     return Material(
-      color: Theme.of(context).colorScheme.background,
-      child: user == null
-          ? const CircularProgressIndicator()
-          : Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(children: <Widget>[
-                const Header(text: 'Account Screen'),
-                const SizedBox(height: 36),
-                ClipOval(
-                  child: user.photoURL != null
-                      ? Image.network(
-                          user.photoURL!,
-                          width: 120,
-                          height: 120,
-                          fit: BoxFit.cover,
-                        )
-                      : Image.network(
-                          'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png',
-                          width: 120,
-                          height: 120,
-                          fit: BoxFit.cover,
-                        ),
-                ),
-                const SizedBox(height: 36),
-                PlainCard(
-                    child: _textAccount(
-                        context: context, text: user.displayName!)),
-                const SizedBox(height: 12),
-                PlainCard(
-                    child: _textAccount(context: context, text: user.email!)),
-                const SizedBox(height: 12),
-                PlainCard(
-                    child: _textAccount(
-                        context: context,
-                        text: user.phoneNumber != null &&
-                                user.phoneNumber!.isNotEmpty
-                            ? user.phoneNumber!
-                            : 'No phone number')),
-                const SizedBox(height: 12),
-                GestureDetector(
-                  onTap: () => FireAuth.signOut(context),
-                  child: PlainCard(
-                      color: Theme.of(context).colorScheme.error,
-                      child: Row(
+        color: Theme.of(context).colorScheme.background,
+        child: userRef.when(
+            data: (data) {
+              final user = data.first;
+              return Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(children: <Widget>[
+                      const Header(text: 'Account Screen'),
+                      const SizedBox(height: 36),
+                      Stack(
                         children: [
-                          const Icon(
-                            Ionicons.log_out_outline,
-                            color: Colors.white,
+                          ClipOval(
+                            child: user.imageUrl != null
+                                ? Image.network(
+                                    user.imageUrl!,
+                                    width: 120,
+                                    height: 120,
+                                    fit: BoxFit.cover,
+                                  )
+                                : Image.network(
+                                    'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png',
+                                    width: 120,
+                                    height: 120,
+                                    fit: BoxFit.cover,
+                                  ),
                           ),
-                          const SizedBox(width: 12),
-                          _textAccount(
-                              context: context,
-                              text: 'Logout',
-                              color: Colors.white),
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: GestureDetector(
+                              onTap: () async {
+                                try {
+                                  String photoUrl = "";
+                                  if (user.imageUrl != null) {
+                                    photoUrl = user.imageUrl!;
+                                  }
+                                  await funcUserRef
+                                      .updateProfilePhoto(photoUrl);
+                                } catch (e) {
+                                  showDialog(
+                                      context: context,
+                                      builder: (_) => AlertDialog(
+                                          content: Text(e.toString())));
+                                }
+                              },
+                              child: ClipOval(
+                                child: Container(
+                                  padding: const EdgeInsets.all(6),
+                                  color: Colors.white,
+                                  child: const Icon(
+                                    Ionicons.camera_outline,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ],
-                      )),
-                )
-                // const SizedBox(height: 12),
-              ]),
-            ),
-    );
+                      ),
+                      const SizedBox(height: 36),
+                      PlainCard(
+                          child:
+                              _textAccount(context: context, text: user.name!)),
+                      const SizedBox(height: 12),
+                      PlainCard(
+                          child:
+                              _textAccount(context: context, text: user.email)),
+                      const SizedBox(height: 12),
+                      GestureDetector(
+                        onTap: () {
+                          FireAuth.signOut();
+                          context.pushReplacement("/login");
+                        },
+                        child: PlainCard(
+                            color: Theme.of(context).colorScheme.error,
+                            child: Row(
+                              children: [
+                                const Icon(
+                                  Ionicons.log_out_outline,
+                                  color: Colors.white,
+                                ),
+                                const SizedBox(width: 12),
+                                _textAccount(
+                                    context: context,
+                                    text: 'Logout',
+                                    color: Colors.white),
+                              ],
+                            )),
+                      )
+                      // const SizedBox(height: 12),
+                    ]),
+                  ),
+                  userAction.when(
+                      data: (act) => Container(),
+                      loading: () =>
+                          const Center(child: CircularProgressIndicator()),
+                      error: (error, stack) => Container()),
+                ],
+              );
+            },
+            error: (e, s) => Center(
+                    child: Container(
+                  child: Text(e.toString()),
+                )),
+            loading: () => const Center(child: CircularProgressIndicator())));
   }
 
   Widget _textAccount({required context, required String text, Color? color}) {

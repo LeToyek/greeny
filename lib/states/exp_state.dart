f5d887b63c334e88cf9e3c6def9b71955e1b20bf
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-07-18 21:30:44 +0700

    feat: init exp

---
+++ lib/states/exp_state.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:greenify/services/users.dart';
+
+class ExpNotifier extends StateNotifier<AsyncValue<String>> {
+  UsersServices usersServices;
+  ExpNotifier({required this.usersServices}) : super(const AsyncValue.data(""));
+
+  Future<void> increaseExp(int value) async {
+    try {
+      state = const AsyncValue.loading();
+      await usersServices.increaseExpUser(value);
+      state = const AsyncValue.data("Success");
+    } catch (e) {
+      state = AsyncValue.error(e, StackTrace.current);
+    }
+  }
+
+  Future<void> levelUp() async {
+    try {
+      state = const AsyncValue.loading();
+      await usersServices.increaseLevelUser();
+      state = const AsyncValue.data("Success");
+    } catch (e) {
+      state = AsyncValue.error(e, StackTrace.current);
+    }
+  }
+}
+
+final expProvider =
+    StateNotifierProvider<ExpNotifier, AsyncValue<String>>((ref) {
+  return ExpNotifier(usersServices: UsersServices());
+});

a44b0faeb260bdf377915d9bda150b56311f40b3
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-07-19 17:36:29 +0700

    fix: back button navigate

--- lib/ui/screen/garden/garden_pot_detail_screen.dart
+++ lib/ui/screen/garden/garden_pot_detail_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:greenify/model/plant_model.dart';
-import 'package:greenify/states/file_notifier.dart';
 import 'package:greenify/states/plant_avatar_state.dart';
 import 'package:greenify/states/pot_state.dart';
 import 'package:greenify/states/scheduler/schedule_picker_state.dart';
@@ -113,133 +111,140 @@ class _GardenPotDetailScreenState extends ConsumerState<GardenPotDetailScreen> {
         error: (error, stackTrace) => Center(child: Text(error.toString())),
         data: (data) {
           final pot = data.first;
-          return NestedScrollView(
-              headerSliverBuilder: (context, innerBoxIsScrolled) {
-                return [
-                  SliverAppBar(
-                    expandedHeight: 200,
-                    pinned: true,
-                    flexibleSpace: FlexibleSpaceBar(
-                      title: Text(
-                        pot.plant.name,
-                        style: textTheme.titleLarge!
-                            .apply(color: Colors.white, fontWeightDelta: 2),
-                      ),
-                      background: Stack(
-                        clipBehavior: Clip.none,
-                        fit: StackFit.expand,
-                        children: [
-                          Image.network(
-                            pot.plant.image,
-                            fit: BoxFit.cover,
-                          ),
-                          const DecoratedBox(
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                begin: Alignment(0.0, 0.5),
-                                end: Alignment(0.0, 0.0),
-                                colors: <Color>[
-                                  Color(0x60000000),
-                                  Color(0x00000000),
-                                ],
-                              ),
+          return WillPopScope(
+            onWillPop: () async {
+              potController.turnBackData();
+              context.pop();
+              return false;
+            },
+            child: NestedScrollView(
+                headerSliverBuilder: (context, innerBoxIsScrolled) {
+                  return [
+                    SliverAppBar(
+                      expandedHeight: 200,
+                      pinned: true,
+                      flexibleSpace: FlexibleSpaceBar(
+                        title: Text(
+                          pot.plant.name,
+                          style: textTheme.titleLarge!
+                              .apply(color: Colors.white, fontWeightDelta: 2),
+                        ),
+                        background: Stack(
+                          clipBehavior: Clip.none,
+                          fit: StackFit.expand,
+                          children: [
+                            Image.network(
+                              pot.plant.image,
+                              fit: BoxFit.cover,
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    leading: IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () {
-                        potController.turnBackData();
-                        context.pop();
-                      },
-                    ),
-                  ),
-                ];
-              },
-              body: Material(
-                  color: Theme.of(context).colorScheme.surface,
-                  child: CustomScrollView(
-                    slivers: [
-                      SliverList(
-                          delegate: SliverChildListDelegate([
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        CircleAvatar(
-                          radius: 45,
-                          child: CircleAvatar(
-                              radius: 40,
-                              backgroundImage: NetworkImage(
-                                _characterImages.firstWhere((element) =>
-                                    element['name'] ==
-                                    pot.plant.category)['image'],
-                              )),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                // plantChoose(pageController, context, ref,
-                                //     _characterImages),
-                                Text(pot.plant.name,
-                                    style: textTheme.titleLarge!
-                                        .apply(fontWeightDelta: 2)),
-
-                                const SizedBox(height: 4),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    plantHeightPill(pot.plant.height),
-                                    const SizedBox(width: 8),
-                                    plantStatusPill(pot.plant.status),
+                            const DecoratedBox(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  begin: Alignment(0.0, 0.5),
+                                  end: Alignment(0.0, 0.0),
+                                  colors: <Color>[
+                                    Color(0x60000000),
+                                    Color(0x00000000),
                                   ],
                                 ),
-                                const SizedBox(height: 4),
-                                Text(
-                                  pot.plant.description,
-                                  style: textTheme.titleMedium!.apply(
-                                      fontWeightDelta: 1, color: Colors.grey),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ]),
+                              ),
+                            ),
+                          ],
                         ),
-                        const SizedBox(
-                          height: 4,
-                        ),
-                        Padding(
+                      ),
+                      leading: IconButton(
+                        icon: const Icon(Icons.arrow_back),
+                        onPressed: () {
+                          potController.turnBackData();
+                          context.pop();
+                        },
+                      ),
+                    ),
+                  ];
+                },
+                body: Material(
+                    color: Theme.of(context).colorScheme.surface,
+                    child: CustomScrollView(
+                      slivers: [
+                        SliverList(
+                            delegate: SliverChildListDelegate([
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          CircleAvatar(
+                            radius: 45,
+                            child: CircleAvatar(
+                                radius: 40,
+                                backgroundImage: NetworkImage(
+                                  _characterImages.firstWhere((element) =>
+                                      element['name'] ==
+                                      pot.plant.category)['image'],
+                                )),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  // plantChoose(pageController, context, ref,
+                                  //     _characterImages),
+                                  Text(pot.plant.name,
+                                      style: textTheme.titleLarge!
+                                          .apply(fontWeightDelta: 2)),
+
+                                  const SizedBox(height: 4),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      plantHeightPill(pot.plant.height),
+                                      const SizedBox(width: 8),
+                                      plantStatusPill(pot.plant.status),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 4),
+                                  Text(
+                                    pot.plant.description,
+                                    style: textTheme.titleMedium!.apply(
+                                        fontWeightDelta: 1, color: Colors.grey),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ]),
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: wateringSchedule(
+                                context,
+                                int.parse(pot.plant.wateringSchedule),
+                                funcScheduleController,
+                                TimeOfDay.fromDateTime(DateTime.parse(
+                                    "2021-10-10 ${pot.plant.wateringTime}")),
+                                funcTimeController),
+                          ),
+                        ])),
+                        SliverPadding(
                           padding: const EdgeInsets.all(8.0),
-                          child: wateringSchedule(
-                              context,
-                              int.parse(pot.plant.wateringSchedule),
-                              funcScheduleController,
-                              TimeOfDay.fromDateTime(DateTime.parse(
-                                  "2021-10-10 ${pot.plant.wateringTime}")),
-                              funcTimeController),
-                        ),
-                      ])),
-                      SliverPadding(
-                        padding: const EdgeInsets.all(8.0),
-                        sliver: SliverGrid.builder(
-                          itemCount: 2,
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            childAspectRatio: 9 / 16,
-                            crossAxisSpacing: 8,
-                            mainAxisSpacing: 8,
+                          sliver: SliverGrid.builder(
+                            itemCount: 2,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              childAspectRatio: 9 / 16,
+                              crossAxisSpacing: 8,
+                              mainAxisSpacing: 8,
+                            ),
+                            itemBuilder: (context, index) => Container(
+                              color: Colors.red.shade100,
+                              width: 100,
+                              height: 100,
+                            ),
                           ),
-                          itemBuilder: (context, index) => Container(
-                            color: Colors.red.shade100,
-                            width: 100,
-                            height: 100,
-                          ),
-                        ),
-                      )
-                    ],
-                  )));
+                        )
+                      ],
+                    ))),
+          );
         });
   }
 }

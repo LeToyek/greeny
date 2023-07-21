ab9663f6b3df114a79bbe11e2ce347ab7ca25a0c
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-07-21 16:56:32 +0700

    fix: book state manager and view detail

--- lib/states/book_state.dart
+++ lib/states/book_state.dart
@@ -4,14 +4,13 @@ import 'package:greenify/services/book_service.dart';
 
 class BookNotifier extends StateNotifier<AsyncValue<List<BookModel>>> {
   final BookServices bookServices;
-  final List<BookModel> tempBooks = [];
+  List<BookModel> tempBooks = [];
   BookNotifier({required this.bookServices}) : super(const AsyncValue.data([]));
 
   Future<void> getAllBooks(String category) async {
     try {
       state = const AsyncValue.loading();
       final books = await bookServices.getBooksFromDB(category);
-      tempBooks.addAll(books);
       state = AsyncValue.data(books);
     } catch (e) {
       state = AsyncValue.error(e, StackTrace.current);
@@ -32,7 +31,6 @@ class BookNotifier extends StateNotifier<AsyncValue<List<BookModel>>> {
     try {
       state = const AsyncValue.loading();
       final createdBook = await bookServices.insertBookToDB(book);
-      print(createdBook);
       state = AsyncValue.data(tempBooks);
     } catch (e) {
       state = AsyncValue.error(e, StackTrace.current);
--- lib/ui/screen/book/book_list_screen.dart
+++ lib/ui/screen/book/book_list_screen.dart
@@ -11,52 +11,38 @@ class BookListScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final bookRef = ref.watch(bookFamilyProvider(category));
 
-    return NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-              SliverAppBar(
-                actions: const [],
-                centerTitle: true,
-                title: Text(
-                  "Artikel $category",
-                ),
-              ),
-            ],
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: AppBar(title: Text(category), centerTitle: true),
         body: SingleChildScrollView(
           child: Material(
               color: Theme.of(context).colorScheme.background,
               child: bookRef.when(
                   data: (data) => Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListView.builder(
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: data.length,
-                            shrinkWrap: true,
-                            itemBuilder: (context, index) {
-                              return FutureBuilder(
-                                  future: data[index].getUserModel(),
-                                  builder: (context, snapshot) {
-                                    if (snapshot.data != null) {
-                                      return Column(
-                                        children: [
-                                          ListTile(
-                                            title: Text(data[index].title),
-                                            subtitle: Text(
-                                                "Penulis: ${snapshot.data!.name!}"),
-                                            leading: Image.network(
-                                                data[index].imageUrl),
-                                            onTap: () {
-                                              context.push(
-                                                  "/book/detail/${data[index].id}");
-                                            },
-                                          ),
-                                          const SizedBox(height: 10),
-                                        ],
-                                      );
-                                    }
-                                    return Container();
-                                  });
-                            }),
-                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: data.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                ListTile(
+                                  tileColor:
+                                      Theme.of(context).colorScheme.surface,
+                                  title: Text(data[index].title),
+                                  subtitle: Text(
+                                      "Penulis: ${data[index].user!.name}"),
+                                  leading: Image.network(data[index].imageUrl),
+                                  onTap: () {
+                                    context
+                                        .push("/book/detail/${data[index].id}");
+                                  },
+                                ),
+                                const SizedBox(height: 10),
+                              ],
+                            );
+                          })),
                   error: (error, stack) => Center(
                         child: Text(error.toString()),
                       ),

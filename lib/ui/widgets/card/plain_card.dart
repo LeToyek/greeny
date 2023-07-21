5c364f503bc6730485a8e8b79a7b7dda5724de66
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-07-21 20:54:02 +0700

    feat: update account detail page

--- lib/ui/widgets/card/plain_card.dart
+++ lib/ui/widgets/card/plain_card.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class PlainCard extends StatelessWidget {
   final Widget child;
   Color? color;
-  PlainCard({super.key, required this.child, this.color});
+  EdgeInsets? padding;
+  PlainCard({super.key, required this.child, this.color, this.padding});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      padding:
+          padding ?? const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
       decoration: BoxDecoration(
         color: color ?? Theme.of(context).colorScheme.surface,
         borderRadius: BorderRadius.circular(8),

559c318a8bb44ab900b98ffb4d2f5323fa660915
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-08-07 18:44:34 +0700

    feat: rename state files

--- lib/states/notification_state.dart
+++ lib/states/notification_state.dart
@@ -1,5 +1,7 @@
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:greenify/services/background_service.dart';
 import 'package:greenify/utils/notification_helper.dart';
+import 'package:hive/hive.dart';
 import 'package:riverpod/riverpod.dart';
 
 final BackgroundServices _backgroundServices = BackgroundServices();
@@ -18,9 +20,19 @@ class NotificationNotifier extends StateNotifier<void> {
     selectNotificationSubject.close();
     didReceiveLocalNotificationSubject.close();
   }
+
+  Future<void> initRealNotification() async {
+    final box = Hive.box('prefs');
+    final statusNotifier = box.get('status_notifier', defaultValue: 0);
+    if (statusNotifier == 0) {
+      box.put('status_notifier', 1);
+      await AndroidAlarmManager.oneShot(
+          Duration.zero, 99999999, BackgroundServices.initCallback);
+    }
+  }
 }
 
 final notificationProvider =
     StateNotifierProvider<NotificationNotifier, void>((ref) {
-  return NotificationNotifier();
+  return NotificationNotifier()..initRealNotification();
 });
